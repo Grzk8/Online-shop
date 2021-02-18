1b18bc3040f250c7b42a6247fb6f6d1b6de748b0
Grzegorz Kaczor <grzesiek125@op.pl>
2021-02-18 13:00:57 +0100

    adding cart model

--- models/cart.js
+++ models/cart.js
@@ -1,7 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
 module.exports = class Cart {
-    constructor() {
-        this.products = [];
-        this.totalPrice = 0;
+    static addProduct(id) {
+        // fetch the previous cart
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 }
+            if (!err) {
+                cart = JSON.parse(fileContent);
+            };
+        // analyze the cart => find existing product
+        const existingProduct = cart.products.find(prod => prod.id === id);
+        
+        });
     };
     
 };
