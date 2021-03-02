73d6aff4e3539d9563a3f081ecec23759c92742b
Grzegorz Kaczor <grzesiek125@op.pl>
2021-03-02 12:59:57 +0100

    deleting items

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,8 +13,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(null, title, imageUrl, price, description);
     product.save();
     res.redirect('/');
 };
@@ -39,7 +38,20 @@ exports.getEditProduct = (req, res, next) => {
   };
 
 exports.postEditProduct = (req, res, next) => {
-    
+    const prodId = req.body.prodId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const updatedProduct = new Product(
+      prodId,
+      updatedTitle,
+      updatedImageUrl,
+      updatedDescription,
+      updatedPrice
+    );
+    updatedProduct.save();
+    res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
@@ -50,4 +62,10 @@ exports.getProducts = (req, res, next) => {
             path: '/admin/products'
         });
     });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const productId = req.body.productId;
+  Product.deleteById(productId);
+  res.redirect('/admin/products');
 };
