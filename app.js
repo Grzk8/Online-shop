5f2b227c983225da2d54045722e99e8ec74c4e64
Grzegorz Kaczor <grzesiek125@op.pl>
2021-03-10 21:03:15 +0100

    connecting sql to database

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.set('views', 'views');
 const admminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+db.execute('SELEST * FROM products');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'j123db'
+});
+
+module.exports = pool.promise();
