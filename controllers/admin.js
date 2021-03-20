af7fa9a2faba0672df0f9309b2d7159006b0865a
Grzegorz Kaczor <grzesiek125@op.pl>
2021-03-20 18:28:50 +0100

    deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,6 +22,8 @@ exports.postAddProduct = (req, res, next) => {
     }).
     then(result => {
       //console.log(result);
+      console.log('created products');
+      res.redirect('/admin/products');
     })
     .catch(err => {
       console.log(err);
@@ -86,6 +88,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product => {
+    return product.destroy();
+  })
+  .then(result => {
+    console.log('Destroyed');
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err));
 };
