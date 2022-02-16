aaa4284ad70a0019c49fcd95a0e61ddffd253de4
Grzegorz Kaczor <grzesiek125@op.pl>
2022-02-16 12:02:27 +0100

    mongoose connected and shema

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -32,6 +32,8 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://user1:user1haslo@cluster0.wcshf.mongodb.net/shop?retryWrites=true').then(result => {
     app.listen(3000);
+}).catch(err => {
+    console.log(err);
 })
