576528f572f57735ccec7c85c9d4070a0b2e3b83
Grzegorz Kaczor <grzesiek125@op.pl>
2022-02-23 18:42:19 +0100

    orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -81,7 +81,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-    req.user.getOrders()
+    Order.find({'user.userId': req.user._id})
     .then(orders => {
         res.render('shop/orders', {
             pageTitle: 'Your Orders',
@@ -99,7 +99,7 @@ exports.postOrder = (req, res, next) => {
     .then(user => {
         console.log(user.cart.items)
         const products = user.cart.items.map(i => {
-            return {quantity: i.quantity, product: i.productId};
+            return {quantity: i.quantity, product: {...i.productId._doc}};
         });
         const order = new Order ({
             user: {
@@ -111,6 +111,9 @@ exports.postOrder = (req, res, next) => {
         order.save();
     })
     .then(result => {
+        return req.user.clearCart();
+    })
+    .then(() => {
         res.redirect('/orders');
     })
     .catch(err => console.log(err));
--- models/order.js
+++ models/order.js
@@ -4,13 +4,13 @@ const Shema = mongoose.Schema;
 
 const orderShema = new Shema ({
     products: [{
-        productData: {type: Object, required: true},
+        product: {type: Object, required: true},
         quantity: {type:Number, required: true}
     }],
     user: {
         name: {type: String, required: true},
-    },
+    
     userId: {type: Shema.Types.ObjectId, required: true, ref: 'User'}
-});
+}});
 
 module.exports = mongoose.model('Order', orderShema);
