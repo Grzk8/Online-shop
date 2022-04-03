6418ef1cea4672e9acad3f3fd755c436a6b04892
Grzegorz Kaczor <grzesiek125@op.pl>
2022-04-03 15:55:52 +0200

    error messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,18 +8,30 @@ exports.getLogin = (req, res, next) => {
 // .split(';')[1]
 // .trim()
 // .split('=')[1];  
+   let message = req.flash('error');
+   if (message.length > 0) {
+      message = message[0];
+   } else {
+      message = null;
+   }
  res.render('auth/login', {
     pageTitle: 'Login',
     path: '/login',
-    isAuthenticated: false
+    errorMessage: message
  })
 };
 
 exports.getSignup = (req, res, next) => {
+   let message = req.flash('error');
+   if (message.length > 0) {
+      message = message[0];
+   } else {
+      message = null;
+   }
    res.render('auth/signup', {
      path: '/signup',
      pageTitle: 'Signup',
-     isAuthenticated: false
+     errorMessage: message
    });
  };
 
@@ -29,6 +41,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({email: email})
   .then(user => {
      if(!user) {
+         req.flash('error', 'Invalid email or password');
          return res.redirect('/login');
      }
    bcrypt.compare(password, user.password)
@@ -41,6 +54,7 @@ exports.postLogin = (req, res, next) => {
             res.redirect('/');
          });
       }
+      req.flash('error', 'Invalid email or password');
       res.redirect('login');
    }).catch(err => {
       console.log(err)
@@ -60,6 +74,7 @@ exports.postSignup = (req, res, next) => {
    User.findOne({email: email})
    .then(userDoc => {
       if (userDoc) {
+         req.flash('error', 'Email already exists');
          return res.redirect('/signup');
       }
       return bcrypt
--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (!req.session.isLoggetIn) {
+    if (!req.session.isLoggedIn) {
         return res.redirect('/login');
     }
     next();
