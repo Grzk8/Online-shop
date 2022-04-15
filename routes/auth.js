dc075d10b53e26d038c29388701c8dc28b3661c6
Grzegorz Kaczor <grzesiek125@op.pl>
2022-04-15 23:18:43 +0200

    valid

--- routes/auth.js
+++ routes/auth.js
@@ -10,11 +10,48 @@ router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 
 router.post('/login',[
-    body('email').isEmail().withMessage('Incorrect email adress'),
-    body('password', 'Invalid password').isLength({min: 5}).isAlphanumeric()
+    body('email').isEmail().withMessage('Incorrect email adress').normalizeEmail(),
+    body('password', 'Invalid password').isLength({min: 5}).isAlphanumeric().trim()
 ] , authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email'), authController.postSignup);
+router.post(
+    '/signup',
+    [
+      check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, { req }) => {
+          // if (value === 'test@test.com') {
+          //   throw new Error('This email address if forbidden.');
+          // }
+          // return true;
+          return User.findOne({ email: value }).then(userDoc => {
+            if (userDoc) {
+              return Promise.reject(
+                'E-Mail exists already, please pick a different one.'
+              );
+            }
+          });
+        })
+        .normalizeEmail(),
+      body(
+        'password',
+        'Please enter a password with only numbers and text and at least 5 characters.'
+      )
+        .isLength({ min: 5 })
+        .isAlphanumeric()
+        .trim(),
+      body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
+    ],
+    authController.postSignup
+  );
 
 router.post('/logout', authController.postLogout);
 
